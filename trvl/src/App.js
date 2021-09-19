e5767cb8d1d6eb0400297f25986c2935540f57e5
Sid Nair <siddharthnair87@outlook.com>
2021-09-19 11:36:35 +0100

    added logo icon

--- trvl/src/App.js
+++ trvl/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Navbar from "./components/Navbar/index";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
