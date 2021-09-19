886b58637e826f5d25f0faddf4105a2c226edc6f
Sid Nair <siddharthnair87@outlook.com>
2021-09-19 11:51:50 +0100

    added Link to home

--- trvl/src/components/Navbar/index.js
+++ trvl/src/components/Navbar/index.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       <nav className="navbar">
@@ -8,6 +13,16 @@ function Navbar() {
           <Link to="/" className="navbar-logo">
             TRVL <i className="fab fa-pagelines" />
           </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
